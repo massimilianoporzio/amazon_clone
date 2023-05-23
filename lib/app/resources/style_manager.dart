a376ed30889e26394fbaf9cb4f6acd0220f49c42
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-05-23 19:35:43 +0200

    auth route

---
+++ lib/app/resources/style_manager.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StyleManager {
+  static const kTitleTextStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 25,
+    letterSpacing: 1,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const kSubtitleTextStyle = TextStyle(
+    color: Colors.black38,
+    fontSize: 16,
+    letterSpacing: 1,
+    fontWeight: FontWeight.w500,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:amazon_clone/app/resources/color_manager.dart';
+import 'package:amazon_clone/app/routes/router.dart';
+import 'package:amazon_clone/features/auth/presentation/pages/auth_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,34 +14,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Amazon Clone',
-        theme: ThemeData(
-          scaffoldBackgroundColor: ColorManager.backgroundColor,
-          colorScheme:
-              const ColorScheme.light(primary: ColorManager.motoSecondaryColor),
-          appBarTheme: const AppBarTheme(
-            elevation: 0,
-            //FISSA IL COLORE DELLE ICONE A NERO ANCHE SE CAMBIO LO SFONDO
-            iconTheme: IconThemeData(color: Colors.black),
-          ),
-          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: ColorManager.backgroundColor,
+        colorScheme:
+            const ColorScheme.light(primary: ColorManager.motoSecondaryColor),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          //FISSA IL COLORE DELLE ICONE A NERO ANCHE SE CAMBIO LO SFONDO
+          iconTheme: IconThemeData(color: Colors.black),
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Hello"),
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      // home: const NotFound404Error(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Text("Flutter Demo Home Page"),
+              Builder(builder: (context) {
+                return ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, AuthScreen.routeName);
+                  },
+                  child: const Text("Click"),
+                );
+              })
+            ],
           ),
-          body: Center(
-            child: Column(
-              children: [
-                const Text("Flutter Demo Home Page"),
-                ElevatedButton(onPressed: () {}, child: const Text("OK"))
-              ],
-            ),
-          ),
-        ));
+        ),
+      ),
+    );
   }
 }
-
-
