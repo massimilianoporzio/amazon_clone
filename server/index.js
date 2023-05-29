29a84f6e34d76a5a33c934cafc76fa028b89e4ff
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-05-29 21:24:20 +0200

    env per mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 console.log("Hello, World from Node Server");
 
-
+//IMPORTS FROM PACKAGES
 const express = require('express');
+const mongoose = require('mongoose');
 
+//IMPORT FROM OTHER FILES
+const authRouter = require('./routes/auth');
+//INIT
 const PORT = 3000; //porta in ascolto
 
 const app = express();
+//middleware
+//CLIENT -> MIDDLEWARE -> SERVER -> MIDDLEWARE -> CLIENT
+app.use(authRouter.authRouter);
+
+//connections
+mongoose.connect("").then(()=>{
+    console.log("Connection to MongoDB successfull");
+}).catch(e => {
+    console.log(e);
+});
+
+
 
 //creating API 0.0.0.0 = from Everywhere
 app.listen(PORT,"0.0.0.0", () => {
