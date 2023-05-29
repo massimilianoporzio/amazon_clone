37e33a31846a1cde87a9cc666869ce7fbd5b21f5
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-05-29 18:56:25 +0200

    again

--- server/index.js
+++ server/index.js
@@ -7,15 +7,6 @@ const PORT = 3000; //porta in ascolto
 
 const app = express();
 
-app.get('/hello-world', (req,res)=>{
-    res.json({"hi":"hello world MASSI"});
-});
-
-app.get("/",(req,res)=>{
- res.json({
-    "name":"Massimiliano"
- });
-});
 //creating API 0.0.0.0 = from Everywhere
 app.listen(PORT,"0.0.0.0", () => {
     console.log(`connected at port ${PORT} hello`);
