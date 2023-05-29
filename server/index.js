3d34531a0ff2fad91538ea01bee9cb3438456b85
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-05-29 18:33:54 +0200

    node

---
+++ server/index.js
@@ -0,0 +1,12 @@
+console.log("Hello, World from Node Server");
+
+
+const express = require('express');
+
+const PORT = 3000; //porta in ascolto
+
+const app = express();
+//creating API 0.0.0.0 = from Everywhere
+app.listen(PORT,"0.0.0.0", () => {
+    console.log(`connected at port ${PORT}`);
+})
